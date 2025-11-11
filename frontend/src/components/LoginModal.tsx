6ac90e18d005adfec79ad40fe7b938f889f92ef6
kaleel <pixelhubster@gmail.com>
2025-11-11 23:40:00 +0000

    fix: added aptos character

--- frontend/src/components/LoginModal.tsx
+++ frontend/src/components/LoginModal.tsx
@@ -90,7 +90,7 @@ export default function LoginModal() {
 
           <div className="text-center mt-4">
             <p className="text-sm text-gray-600">
-              Don't have an account?{' '}
+              Don&apos;t have an account?{' '}
               <button
                 type="button"
                 onClick={handleSwitchToRegister}
