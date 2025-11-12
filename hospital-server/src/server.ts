1ccc4f7ea869006f6092d81bd66cd91dcaa79378
kaleel <pixelhubster@gmail.com>
2025-11-12 00:30:27 +0000

    reverted vercel confi fixes

--- hospital-server/src/server.ts
+++ hospital-server/src/server.ts
@@ -1,12 +1,6 @@
 import app from './app';
 import config from './config/config';
 
-// For Vercel serverless functions
-export default app;
-
-// For local development
-if (require.main === module) {
-  app.listen(config.port, () => {
-    console.log(`Server running on port ${config.port}`);
-  });
-}
+app.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`);
+});
